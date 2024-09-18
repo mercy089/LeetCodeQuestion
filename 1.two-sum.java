51a017ff5b9e45ed2c317bc7bc9f08f3de4260ae
Piyush Verma <piyush6166@gmail.com>
2024-09-18 22:48:38 +0530

    first commit

---
+++ 1.two-sum.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        return nums;
+        
+    }
+}
+// @lc code=end
+
